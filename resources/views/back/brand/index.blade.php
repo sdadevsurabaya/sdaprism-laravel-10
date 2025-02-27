bd0149bc94e3bb3ca86f8d22f7df0c3b804895c4
suryo <suryoatm@gmail.com>
2025-02-27 10:32:54 +0700

    remove form search

--- resources/views/back/brand/index.blade.php
+++ resources/views/back/brand/index.blade.php
@@ -38,13 +38,7 @@
                                 <div class="card-title">
                                     <!--begin::Search-->
                                     <div class="d-flex align-items-center position-relative my-1">
-                                        <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
-                                            <span class="path1"></span>
-                                            <span class="path2"></span>
-                                        </i>
-                                        <input type="text" data-kt-ecommerce-order-filter="search"
-                                            class="form-control form-control-solid w-250px ps-12"
-                                            placeholder="Search Order" />
+
                                     </div>
                                     <!--end::Search-->
                                 </div>
