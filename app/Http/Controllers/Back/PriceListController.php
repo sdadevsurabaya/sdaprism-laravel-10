a8c548604ee0ca128419ddc95756e1e47ebd6392
Danu-Nur <diramadhan13@gmail.com>
2025-05-08 09:14:15 +0700

    Add printPDF method to PriceListController for PDF generation

--- app/Http/Controllers/Back/PriceListController.php
+++ app/Http/Controllers/Back/PriceListController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class PriceListController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('back.pricelist.price-list');
+    }
+
+    public function printPDF(Request $request)
+    {
+        $htmlContent = $request->htmltable;
+        $pdf = Pdf::loadView('back.print.print-price-list', $htmlContent);
+        return $pdf->stream('price-list.pdf');
     }
 }
