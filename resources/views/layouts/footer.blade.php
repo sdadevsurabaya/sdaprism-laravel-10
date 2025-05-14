707576b3ed46294559408f54c37303c182e93a34
Danu-Nur <diramadhan13@gmail.com>
2025-05-14 14:30:41 +0700

    Refactor footer scripts to use asset helper for improved path management

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,24 +1,24 @@
  <!-- core:js -->
- <script src="assets/js/core.js"></script>
+ <script src="{{ asset('assets/js/core.js') }}"></script>
  <!-- endinject -->
 
  <!-- Plugin js for this page -->
- <script src="assets/js/flatpickr.min.js"></script>
- <script src="assets/js/apexcharts.min.js"></script>
+ <script src="{{ asset('assets/js/flatpickr.min.js') }}"></script>
+ <script src="{{ asset('assets/js/apexcharts.min.js') }}"></script>
  <!-- End plugin js for this page -->
 
  <!-- inject:js -->
- <script src="assets/js/feather.min.js"></script>
- <script src="assets/js/app.js"></script>
+ <script src="{{ asset('assets/js/feather.min.js') }}"></script>
+ <script src="{{ asset('assets/js/app.js') }}"></script>
  <!-- endinject -->
 
  <!-- Custom js for this page -->
- <script src="assets/js/dashboard.js"></script>
+ <script src="{{ asset('assets/js/dashboard.js') }}"></script>
  <!-- End custom js for this page -->
 
  <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
- <script src="/assets/js/dataTables.js"></script>
- <script src="/assets/js/dataTables.bootstrap5.js"></script>
+ <script src="{{ asset('assets/js/dataTables.js') }}"></script>
+ <script src="{{ asset('assets/js/dataTables.bootstrap5.js') }}"></script>
 
  </body>
 
