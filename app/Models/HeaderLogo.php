7c5a1bf8fd7608fa4aea9a1ec2b3bbe17f6282a5
Danu-Nur <diramadhan13@gmail.com>
2025-05-20 11:58:35 +0700

    Add relationship method to HeaderLogo model for associated pricelists

--- app/Models/HeaderLogo.php
+++ app/Models/HeaderLogo.php
@@ -9,4 +9,9 @@ class HeaderLogo extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function pricelists()
+    {
+        return $this->hasMany(Pricelist::class);
+    }
 }
