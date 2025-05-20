3a2d113a56264f15c396525833c99e858b477a86
Danu-Nur <diramadhan13@gmail.com>
2025-05-20 11:58:25 +0700

    Add relationship method to Currency model for associated pricelists

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -9,4 +9,9 @@ class Currency extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function pricelists()
+    {
+        return $this->hasMany(Pricelist::class);
+    }
 }
