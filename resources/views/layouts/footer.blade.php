36a9bddb0fd33f2d72e10e5a8a87887d7991a249
Danu-Nur <diramadhan13@gmail.com>
2025-06-02 14:12:23 +0700

    Add toastr.js for enhanced user notifications in header and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -19,7 +19,7 @@
  <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
  <script src="{{ asset('assets/js/dataTables.js') }}"></script>
  <script src="{{ asset('assets/js/dataTables.bootstrap5.js') }}"></script>
-
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
  </body>
 
  </html>
