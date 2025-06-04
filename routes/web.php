bd300f0101d19b280f5f29e4f3f0477777c71a1a
Danu-Nur <diramadhan13@gmail.com>
2025-06-04 15:30:10 +0700

    Refactor web routes to improve organization; group routes by middleware and remove redundant declarations

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Back\PriceListController as BackPriceListController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\HeaderLogoController;
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PricelistController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\QuotationController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,33 +22,40 @@ use App\Http\Controllers\RoleController;
 |
 */
 Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'login'])->name('login.submit');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+// ========== HANYA UNTUK ADMIN ==========
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('dashboard', [HomeController::class, 'index'])->name('dashboard');
 
-// Route::get('/', [HomeController::class, 'index']);
-Route::get('dashboard', [HomeController::class, 'index'])->name('dashboard');
+    // Role management
+    Route::resource('roles', RoleController::class);
 
-//pricelist
-Route::get('pricelists', [PricelistController::class, 'index'])->name('pricelists.index');
-Route::get('pricelists/create', [PricelistController::class, 'create'])->name('pricelists.create');
-Route::post('pricelists', [PricelistController::class, 'store'])->name('pricelists.store');
-Route::get('pricelists/{pricelist}/edit', [PricelistController::class, 'edit'])->name('pricelists.edit');
-Route::put('pricelists/{pricelist}', [PricelistController::class, 'update'])->name('pricelists.update');
-Route::delete('pricelists/{pricelist}', [PricelistController::class, 'destroy'])->name('pricelists.destroy');
-Route::get('pricelistPDF', [PricelistController::class, 'template_pdf'])->name('pricelist.template_pdf');
+    // Brand
+    Route::resource('brand', HeaderLogoController::class);
+
+    // Currency
+    Route::resource('currency', CurrencyController::class);
+
+    // Users
+    Route::resource('user', UserController::class);
+});
+
+// ========== UNTUK SEMUA ROLE YANG LOGIN ==========
+Route::middleware(['auth', 'role:admin,staff'])->group(function () {
+    // Pricelists
+    Route::get('pricelists', [PricelistController::class, 'index'])->name('pricelists.index');
+    Route::get('pricelists/create', [PricelistController::class, 'create'])->name('pricelists.create');
+    Route::post('pricelists', [PricelistController::class, 'store'])->name('pricelists.store');
+    Route::get('pricelists/{pricelist}/edit', [PricelistController::class, 'edit'])->name('pricelists.edit');
+    Route::put('pricelists/{pricelist}', [PricelistController::class, 'update'])->name('pricelists.update');
+    Route::delete('pricelists/{pricelist}', [PricelistController::class, 'destroy'])->name('pricelists.destroy');
+    Route::get('pricelistPDF', [PricelistController::class, 'template_pdf'])->name('pricelist.template_pdf');
+});
 
 //quotation
 Route::resource('quotation', QuotationController::class);
 // Route::get('quotationCreate', [QuotationController::class, 'create'])->name('quotation.create');
-
-// Roles
-Route::resource('roles', RoleController::class);
-
-// BRAND
-Route::resource('brand', HeaderLogoController::class);
-
-// CURRENCY
-Route::resource('currency', CurrencyController::class);
-
-Route::resource('user', UserController::class);
 
 Route::get('pricelist-table', [BackPriceListController::class, 'index'])->name('pricelist.table');
 Route::get('pricelist-pdf/{pricelist}', [BackPriceListController::class, 'viewPdf'])->name('pricelist.pdf');
