59984a6ede210c3be3fd7ab304d8d9abafc91700
Danu-Nur <diramadhan13@gmail.com>
2025-06-04 09:13:28 +0700

    Refactor brand management modals; implement create and edit functionality with improved UI and data handling

--- resources/views/master/list-brand-logo.blade.php
+++ resources/views/master/list-brand-logo.blade.php
@@ -22,11 +22,12 @@
             <div class="card-body">
                 <div class="d-flex w-100 justify-content-end">
 
-                    <a href="{{ route('brand.create') }}" type="button"
-                        class="btn btn-outline-primary btn-icon-text me-2 mb-2 mb-md-0">
+                    <button type="button" class="btn btn-outline-primary btn-icon-text me-2 mb-2 mb-md-0"
+                        data-bs-toggle="modal" data-bs-target="#modalCreateBrand">
                         <i class="btn-icon-prepend" data-feather="plus"></i>
                         Add New
-                    </a>
+                    </button>
+
                 </div>
                 <div class="table-responsive">
                     <table id="pricelist" class="table table-responsive">
@@ -52,8 +53,11 @@
                                                 style="width: auto; height: 30px; border-radius: 0;"></div>
                                     </td>
                                     <td>
-                                        <a href="{{ route('brand.edit', $item->id) }}"
-                                            class="btn btn-sm btn-primary btn-icon-text">
+                                        <a href="javascript:void(0);"
+                                            class="btn btn-sm btn-primary btn-icon-text btn-edit-brand"
+                                            data-id="{{ $item->id }}" data-name="{{ $item->name }}"
+                                            data-logo="{{ asset($item->logo_path) }}"
+                                            data-url="{{ route('brand.update', $item->id) }}">
                                             <i class="btn-icon-prepend" data-feather="edit"></i>
                                             Edit
                                         </a>
@@ -72,73 +76,66 @@
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="createUser" tabindex="-1" aria-labelledby="createUserLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered ">
+    <!-- Modal Create Brand -->
+    <div class="modal fade" id="modalCreateBrand" tabindex="-1" aria-labelledby="modalCreateBrandLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="createUserLabel">Create User</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body p-0">
-                    <div class="col-12 grid-margin stretch-card">
-                        <div class="card">
-                            <div class="card-body">
-                                <form class="forms-sample">
-                                    <div class="mb-3">
-                                        <label for="exampleInputUsername1" class="form-label">Username</label>
-                                        <input type="text" class="form-control" id="exampleInputUsername1"
-                                            autocomplete="off" placeholder="Username">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1"
-                                            placeholder="Email">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            autocomplete="off" placeholder="Password">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Departemen</label>
-                                        <input type="text" class="form-control" id="exampleInputUsername1"
-                                            autocomplete="off" placeholder="Departemen">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Jabatan</label>
-                                        <select class="form-select mb-3">
-                                            <option selected="">Select</option>
-                                            <option value="1">Manager </option>
-                                            <option value="2">Supervisor</option>
-                                            <option value="3">Staff</option>
-                                        </select>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Roles</label>
-                                        <select class="form-select mb-3">
-                                            <option selected="">Select</option>
-                                            <option value="1">Admin</option>
-                                            <option value="2">Member</option>
-                                        </select>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="form-label">Status</label>
-                                        <select class="form-select mb-3">
-                                            <option selected="">Select</option>
-                                            <option value="1">Active</option>
-                                            <option value="2">Not Active</option>
-                                        </select>
-                                    </div>
+                <form id="form-create-brand" action="{{ route('brand.store') }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalCreateBrandLabel">Create Brand</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" class="form-control" name="name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Foto</label>
+                            <input type="file" class="form-control" name="logo">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-                                    <button type="submit" class="btn btn-primary me-2">Submit</button>
-                                    <button class="btn btn-secondary">Cancel</button>
-                                </form>
-
+    <!-- Modal Edit Brand -->
+    <div class="modal fade" id="modalEditBrand" tabindex="-1" aria-labelledby="modalEditBrandLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <form id="form-edit-brand" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalEditBrandLabel">Edit Brand</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="edit-brand-id">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" class="form-control" name="name" id="edit-brand-name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Foto</label>
+                            <input type="file" class="form-control" name="logo" id="edit-brand-logo">
+                            <div class="mt-2">
+                                <img id="edit-brand-preview" src="" width="100" height="auto">
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -146,6 +143,24 @@
 
 @push('scripts')
     <script>
-        new DataTable('#pricelist');
+        document.addEventListener('DOMContentLoaded', function() {
+            new DataTable('#pricelist');
+            document.querySelectorAll('.btn-edit-brand').forEach(button => {
+                button.addEventListener('click', function() {
+                    const id = this.dataset.id;
+                    const name = this.dataset.name;
+                    const logo = this.dataset.logo;
+                    const url = this.dataset.url;
+
+                    document.querySelector('#form-edit-brand').action = url;
+                    document.querySelector('#edit-brand-name').value = name;
+                    document.querySelector('#edit-brand-preview').src = logo;
+
+                    const editModal = new bootstrap.Modal(document.getElementById(
+                        'modalEditBrand'));
+                    editModal.show();
+                });
+            });
+        });
     </script>
 @endpush
